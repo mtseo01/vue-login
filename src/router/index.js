22c4ddc63fc870ae5a3a1b88273ca6bb2a1cdefb
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-12-23 18:01:12 +0900

    Feat: Set Router Guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '../store/store'
 
+const rejectAuthUser = (to, from, next) => {
+  if (store.state.isLogin === true) {
+    alert('이미 로그인을 하였습니다.')
+    next('/')
+  } else next()
+}
+const onlyAuthUser = (to, from, next) => {
+  if (store.state.isLogin === false) {
+    alert('로그인이 필요합니다.')
+    next('/login')
+  } else next()
+}
 const routes = [
   {
     path: '/',
@@ -10,12 +23,14 @@ const routes = [
   {
     path: '/login',
     name: 'login',
+    beforeEnter: rejectAuthUser,
     component: () =>
       import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
   },
   {
     path: '/mypage',
     name: 'myPage',
+    beforeEnter: onlyAuthUser,
     component: () =>
       import(/* webpackChunkName: "mypage" */ '../views/MyPageView.vue')
   }

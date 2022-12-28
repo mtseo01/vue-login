bae52646986a6d18949bb7385242a1edf2a5fada
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-12-28 19:42:02 +0900

    Feat: Share cookies (back-end)

--- server/server.js
+++ server/server.js
@@ -5,25 +5,32 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const allUsers = [
   { id: 1, name: 'kim', email: 'kim@gmail.com', password: '123456' },
   { id: 2, name: 'choi', email: 'choi@gmail.com', password: '123456' }
 ];
 
-app.use(cors());
+const cookieOptions = {
+  path: '/',
+  httpOnly: true,
+  sameSite: 'none',
+  secure: true
+};
+
+app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// app.get('/api/users', (req, res) => {
-//   res.send('allUsers');
+// app.get('/api/user', (req, res) => {
+//   if (req.cookies && req.cookies.token) {
+//     const selectedUser = JSON.stringify(req.cookies.token);
+//   }
 // });
 
-app.post('/api/users', (req, res) => {
+app.post('/api/login', (req, res) => {
   const userEmail = req.body.email;
   const userPassword = req.body.password;
   let selectedUser = null;
@@ -32,9 +39,24 @@ app.post('/api/users', (req, res) => {
     if (user.email === userEmail) selectedUser = user;
   });
 
+  // const token = jwt.sign(
+  //   {
+  //     email: selectedUser.email,
+  //     name: selectedUser.name
+  //   },
+  //   'abc1234',
+  //   {
+  //     expiresIn: '15m',
+  //     issuer: 'server-admin'
+  //   }
+  // );
+
   if (selectedUser === null || selectedUser.password !== userPassword) {
     res.send({ success: false });
-  } else res.send({ success: true, selectedUser });
+  } else {
+    res.cookie('token', JSON.stringify(selectedUser), cookieOptions);
+    res.send({ success: true, selectedUser });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {

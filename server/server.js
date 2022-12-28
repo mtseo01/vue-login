df8ab18373c1c9a3f7cb8636f7b43d0eedfa2981
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2022-12-28 22:09:40 +0900

    Feat: Share cookies using JWT & Auth user API

--- server/server.js
+++ server/server.js
@@ -24,11 +24,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// app.get('/api/user', (req, res) => {
-//   if (req.cookies && req.cookies.token) {
-//     const selectedUser = JSON.stringify(req.cookies.token);
-//   }
-// });
+app.get('/api/auth', (req, res) => {
+  console.log(req.cookies);
+  if (req.cookies && req.cookies.token) {
+    jwt.verify(req.cookies.token, 'abc1234', (err, decoded) => {
+      if (err) {
+        console.log('만료된 토큰입니다.');
+        return res.sendStatus(401);
+      } else {
+        console.log(decoded);
+        res.send(decoded);
+      }
+    });
+  } else {
+    console.log('토큰이 없습니다.');
+    return res.sendStatus(401);
+  }
+});
 
 app.post('/api/login', (req, res) => {
   const userEmail = req.body.email;
@@ -39,22 +51,22 @@ app.post('/api/login', (req, res) => {
     if (user.email === userEmail) selectedUser = user;
   });
 
-  // const token = jwt.sign(
-  //   {
-  //     email: selectedUser.email,
-  //     name: selectedUser.name
-  //   },
-  //   'abc1234',
-  //   {
-  //     expiresIn: '15m',
-  //     issuer: 'server-admin'
-  //   }
-  // );
+  const token = jwt.sign(
+    {
+      email: selectedUser.email,
+      name: selectedUser.name
+    },
+    'abc1234',
+    {
+      expiresIn: '15m',
+      issuer: 'server-admin'
+    }
+  );
 
   if (selectedUser === null || selectedUser.password !== userPassword) {
     res.send({ success: false });
   } else {
-    res.cookie('token', JSON.stringify(selectedUser), cookieOptions);
+    res.cookie('token', token, cookieOptions);
     res.send({ success: true, selectedUser });
   }
 });

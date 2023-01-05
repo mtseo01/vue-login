99cdf6317ebe976e348269d9d5ad31cb2f3246ae
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-05 11:12:47 +0900

    Chore: Connect DB

--- server/app.js
+++ server/app.js
@@ -5,8 +5,16 @@ dotenv.config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+const mongoose = require('mongoose');
 const userRouter = require('./api/routes/user');
+
+mongoose.set('strictQuery', true);
+mongoose.connect(
+  `mongodb+srv://mtseo:${process.env.DB_PWD}@cluster0.nxorzkg.mongodb.net/vue-toy?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true
+  }
+);
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));

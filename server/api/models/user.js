6e0e41cfa60b7a5243da2bc06817bad39e1430d6
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2023-01-05 11:14:25 +0900

    Add User Model

---
+++ server/api/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /^([\w\.\_\-])*[a-zA-Z0-9]+([\w\.\_\-])*([a-zA-Z0-9])+([\w\.\_\-])+@([a-zA-Z0-9]+\.)+[a-zA-Z0-9]{2,8}$/
+  },
+  password: { type: String, required: true },
+  name: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
